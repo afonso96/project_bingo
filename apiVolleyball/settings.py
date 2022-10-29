e004fdc289efe3f81a02585ffc87421ff03382c8
Supimpa <afonso2011fachineti@hotmail.com>
2022-10-29 16:15:23 -0300

    Config Data Base

--- apiVolleyball/settings.py
+++ apiVolleyball/settings.py
@@ -82,7 +82,6 @@ WSGI_APPLICATION = "apiVolleyball.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
