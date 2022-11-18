b27f6f4c51bb9335f75bae2f5d651cd337d21af6
Supimpa <afonso2011fachineti@hotmail.com>
2022-11-18 10:37:47 -0300

    Add description with swagger in endpoints

--- classes/views.py
+++ classes/views.py
@@ -1,16 +1,26 @@
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
+from drf_spectacular.utils import extend_schema
 
 from classes.models import Training, MatchTraining
 from classes.serializers import TrainingSerializers, MatchTrainingSerializers
 
 
+@extend_schema(
+    request=TrainingSerializers,
+    responses={201: TrainingSerializers},
+    description= "Create training"
+)
 class TrainingViews(viewsets.ModelViewSet):
     serializer_class = TrainingSerializers
     queryset = Training.objects.all()
     pagination_class = PageNumberPagination
 
-
+@extend_schema(
+    request=MatchTrainingSerializers,
+    responses={201: MatchTrainingSerializers},
+    description= "Verify which student go to training"
+)
 class MatchTrainingViews(viewsets.ModelViewSet):
     serializer_class = MatchTrainingSerializers
     queryset = MatchTraining.objects.all()
